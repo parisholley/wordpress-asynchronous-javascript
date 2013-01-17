aeaaf4dcb46e6282e6d6c303252f6fd4f5bc1b9b
Paris Holley <mail@parisholley.com>
2013-01-17 00:17:26 -0600

    initial commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+=== Plugin Name ===
+Contributors: parisholley
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=paris%40holleywoodproductions%2ecom&lc=US&item_name=Paris%20Holley&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHostedGuest
+Tags: async,headjs,asynchronous,performance,javascript,css
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: trunk
+
+Improve page load performance by asynchronously loading javascript and CSS files using head.js
+
+
+== Installation ==
+
+1. Upload `asynchronous-assets` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= How do I include a script that I do not control but has a dependency on another script? =
+
+In your theme, use the static AsynchronousAssets::wp_enqueue_async_script() method, which is API compatible with wp_enqueue_script($handle, $src, $deps). Using this method will cause the $src to load ONLY when scripts with the handles defined in $deps have been loaded.
+
+= Is there any potential problems with other plugins? =
+
+As long as those plugins are using the built-in wordpress script queuing and not attempting to output scripts manually (such as invoking wp_print_scripts()), you should have no problems.
+
+== Changelog ==
+
+= 0.0.1 =
+* Initial release, no CSS support yet. Me so sleepy :(
---
+++ asyncjs.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: Asynchronous Assets (Javascript and CSS)
+Plugin URI: https://github.com/parisholley/Wordpress-Asynchronous-Assets
+Description: Improve page load performance by asynchronously loading javascript and CSS files using head.js
+Version: 1.0
+Author: Paris Holley
+Author URI: http://www.linkedin.com/in/parisholley
+Author Email: mail@parisholley.com
+License:
+
+  Copyright 2013 Paris Holley (mail@parisholley.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+class AsynchronousAssets {
+	private static $queue = array();
+	private static $depends = array();
+	private static $head_loaded = false;
+
+	function init() {
+		add_action('wp_print_scripts', 'AsynchronousAssets::action_prevent_script_output' );
+		add_filter('script_loader_src', 'AsynchronousAssets::filter_queue_script', 10, 2 );
+		add_filter('print_footer_scripts', 'AsynchronousAssets::filter_headjs' );
+		add_filter('print_head_scripts', 'AsynchronousAssets::filter_headjs' );
+	}
+
+	/**
+	 * Prevent wordpress from outputing scripts to page
+	 **/
+	function action_prevent_script_output() {
+		global $wp_scripts;
+
+		$wp_scripts->do_concat = true;
+	}
+	
+
+	/**
+	 * Wordpress has no ability to hook into script queuing, so this is a work around
+	 **/
+	function filter_queue_script($src, $handle) {
+		self::$queue[] = "{'{$handle}': '$src'}";
+	}
+
+	/**
+	 * Outputs headjs code in header or footer
+	 **/
+	function filter_headjs(){
+		if(count(self::$queue) > 0){
+			if(!$head_loaded){
+				echo '<script type="text/javascript" src="' . plugins_url( '/js/head.load.min.js', __FILE__ ) . '"></script>';
+			
+				self::$head_loaded = true;
+			}
+
+			echo '<script type="text/javascript">head.js(' . implode(',', self::$queue) . ')</script>';
+
+			self::$queue = array();
+		}
+
+		if(count(self::$depends) > 0){
+			foreach(self::$depends as $handle => $depend){
+				if(is_array($depend['deps'])){
+					echo '<script type="text/javascript">head.ready("' . implode(',', $depend['deps']) . '", function(){head.js({"' . $handle . '": "' . $depend['src'] . '"})})</script>';
+				}elseif(is_string($depend['deps'])){
+					echo '<script type="text/javascript">head.ready("' . $depend['deps'] . '", function(){head.js({"' . $handle . '": "' . $depend['src'] . '"})})</script>';
+				}else{
+					echo '<script type="text/javascript">head.js({"' . $handle . '": "' . $depend['src'] . '"});</script>';
+				}
+			}
+
+			self::$depends = array();
+		}
+
+		return false; // prevent printing of javascript
+	}
+
+	function wp_enqueue_async_script($handle, $src, $deps){
+		self::$depends[$handle] = array(
+			'src' => $src,
+			'deps' => $deps
+		);
+	}
+}
+
+AsynchronousAssets::init();
